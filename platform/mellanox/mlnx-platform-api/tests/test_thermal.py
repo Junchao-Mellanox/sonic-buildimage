11f8c3f21222378b1802397ced1eed9241440cc7
Junchao-Mellanox <junchao@nvidia.com>
2025-01-08 15:42:33 +0800

    test

--- platform/mellanox/mlnx-platform-api/tests/test_thermal.py
+++ platform/mellanox/mlnx-platform-api/tests/test_thermal.py
@@ -39,7 +39,9 @@ class TestThermal:
     @mock.patch('os.path.exists', mock.MagicMock(return_value=True))
     @mock.patch('sonic_platform.device_data.DeviceDataManager.get_gearbox_count', mock.MagicMock(return_value=2))
     @mock.patch('sonic_platform.device_data.DeviceDataManager.get_cpu_thermal_count', mock.MagicMock(return_value=2))
-    @mock.patch('sonic_platform.device_data.DeviceDataManager.get_sodimm_thermal_count', mock.MagicMock(return_value=2))
+    @mock.patch('sonic_platform.thermal.glob.iglob', mock.MagicMock(
+        return_value=['/run/hw-management/thermal/sodimm1_temp_input',
+                      '/run/hw-management/thermal/sodimm2_temp_input']))
     @mock.patch('sonic_platform.device_data.DeviceDataManager.get_platform_name', mock.MagicMock(return_value='x86_64-mlnx_msn2700-r0'))
     def test_chassis_thermal(self):
         from sonic_platform.thermal import THERMAL_NAMING_RULE
